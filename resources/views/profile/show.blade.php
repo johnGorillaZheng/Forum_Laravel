0c203fd376e57b1673abf114e6bdd19b2a2b4db9
John Zixuan Zheng <u6153124@anu.edu.au>
2017-07-12 17:30:53 +1000

    user profile

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -10,12 +10,30 @@
 
                 <div class="panel-body">
                     <user-avatar avatar="{{Auth::user()->avatar}}"></user-avatar>
-
                 </div>
 
                 <div class="panel-body">
-                    您的昵称：
-                    您的密码：
+                    <form action="">
+                        <div class="form-group">
+                            <label for="changName">修改昵称</label>
+                            <input type="text" name="changName" placeholder="您的新昵称" class="form-control">
+                        </div>
+                       
+                        <div class="form-group">
+                            <label for="changName">修改密码</label>
+                            <div class="form-group">
+                                <label for="oldPasswork">旧密码</label>
+                                <input name="oldPasswork" type="password" class="form-control">
+                                <label for="newPassword">新密码</label>
+                                <input name="newPassword" type="password" class="form-control">
+                                <label for="confirm">请确认</label>
+                                <input name="confirm" type="password" class="form-control">
+                            </div>
+                        </div>
+                        <center>
+                                <button type="submit" class="btn btn-primary">修改资料</button>
+                        </center>
+                    </form>
                 </div>
             </div>
         </div>
